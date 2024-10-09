0c8fe248366649a707263b6c280af14c177b2497
Andrea Gulli <andreagulli11@gmail.com>
2024-10-09 17:56:13 +0200

    complete BONUS 1

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@ class Movie{
     public $title, $director, $releaseYear, $genre, $duration, $rating; // le dichiaro pubbliche in modo che siano visibili e accessibili in qualsiasi parte del codice.
 
     // Definisco il COSTRUTTORE della mia classe, così facendo mi assicuro che le istanze saranno popolate dalle variabili definite nel mio costruttore.
-    function __construct($_title, $_releaseYear, $_genre){
+    function __construct($_title, $_releaseYear, Genre $_genre){
         $this -> title = $_title;
         $this -> releaseYear = $_releaseYear;
         $this -> genre = $_genre;
@@ -13,15 +13,31 @@ class Movie{
 
     // Definisco i METODI della mia classe
     function getMovieInfo(){  // Funzione che restituisce tutte le informazioni del mio metodo
-        return "{$this -> title} {$this -> director} {$this -> releaseYear} {$this -> genre} {$this -> duration} {$this -> rating}";
+        return "{$this -> title} {$this -> director} | Year: {$this -> releaseYear} | Genre: {$this -> genre -> name} {$this -> duration} {$this -> rating}";
     }
 }
 
-// Istanzio due oggetti MOVIE e li stampo a schermo.
-$fastandfuriousXI = new Movie('Fast XI', 2026, 'action');
-$cars4 = new Movie('Cars 4 il ritiro di MCQUEEN', 2028, 'cartoons');
-
 // var_dump($fastandfuriousXI) ;
+
+// Definisco una CLASSE GENRE per effettuare la composizione
+class Genre{
+    // Variabili di istanza pubbliche per la mia classe GENRE.
+    public $name, $description;
+
+    // Costruttore della mia classe GENRE.
+    function __construct($_name, $_description){
+        $this -> name = $_name;
+        $this -> description = $_description;
+    } 
+}
+
+// Istanzio i due OGGETTI della CLASSE GENRE.
+$actionGenre = new Genre('Action', 'Fast-paced films with exciting sequences.');
+$cartoonGenre = new Genre('Cartoons', 'Animated films suitable for children.');
+
+// Istanzio i due OGGETTI della CLASSE MOVIE e li stampo a schermo.
+$fastandfuriousXI = new Movie('Fast XI', 2026, $actionGenre);
+$cars4 = new Movie('Cars 4 il ritiro di MCQUEEN', 2028, $cartoonGenre);
 ?>
  
 <!DOCTYPE html>
