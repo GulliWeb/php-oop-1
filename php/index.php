5ce8ef617daf1e2e21d65471d46e52b2781e3513
Andrea Gulli <andreagulli11@gmail.com>
2024-10-09 15:58:09 +0200

    add basic project files

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<?php
+// # Definisco una classe, da questo momento posso genereare un ISTANZA di un OGGETTO di tipo "MOVIE"
+class Movie{
+
+}
+
+?>
+ 
+ <!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+</head>
+<body>
+</body>
+</html>
